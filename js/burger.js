7c8199d7508324685de4b34a468a4579c1e2c27f
salandre <benoit.salandre@gmail.com>
2022-04-15 13:27:40 +0200

    corrections sur la navbar

--- js/burger.js
+++ js/burger.js
@@ -1,14 +1,14 @@
 let link = document.getElementById('link');
 let burger = document.getElementById('burger');
 let ul = document.querySelector('ul');
-let navup = document.querySelector('div.navup')
-let title = document.querySelector('div.title')
+let navup = document.querySelector('.navup')
+//let title = document.querySelector('.navup.title')
 
 link.addEventListener('click', function(e) {
   e.preventDefault()
   burger.classList.toggle('open');
   ul.classList.toggle('open');
   navup.classList.toggle('open');
-  title.classList.toggle('open')
+//  title.classList.toggle('open')
 })
 
