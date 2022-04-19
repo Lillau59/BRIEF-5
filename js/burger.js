2663ece183d175e022557b7c9eb0780a30570c36
salandre <benoit.salandre@gmail.com>
2022-04-19 22:19:31 +0200

    Correction du problème de pages non scrollables en responsive

--- js/burger.js
+++ js/burger.js
@@ -3,7 +3,7 @@ let burger = document.getElementById('burger');
 let ul = document.querySelector('ul');
 let navup = document.querySelector('.navup');
 let navdown = document.querySelector('.navdown');
-//let title = document.querySelector('.navup.title')
+let body = document.querySelector('body');
 
 link.addEventListener('click', function (e) {
   e.preventDefault()
@@ -11,6 +11,6 @@ link.addEventListener('click', function (e) {
   ul.classList.toggle('open');
   navup.classList.toggle('open');
   navdown.classList.toggle('open');
-  //  title.classList.toggle('open')
+  body.classList.toggle('open');
 })
 
